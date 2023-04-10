90d32e852883f0d05344db4146df2c3b19a38313
Cici <aito.kenza@icloud.com>
2023-04-10 03:54:17 +0200

    maj du projet

--- myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index():
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def my_view(request):
-    return render(request, '/templates/my_template.html')
